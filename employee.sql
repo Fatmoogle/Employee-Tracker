c50e3539c8187e349263ff4fd3f830f1a8bdd520
Alex Varela <alexvar93@gmail.com>
2020-09-16 15:52:48 -0400

    Fixed seed.sql error

--- employee.sql
+++ employee.sql
@@ -6,15 +6,15 @@ USE employee_db;
 
 CREATE TABLE department (
   id INTEGER NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NULL,
+  name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id INTEGER NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NULL,
-  salary DECIMAL(10, 5) NULL,
-  department_id INTEGER NULL,
+  title VARCHAR(30),
+  salary DECIMAL(10, 5),
+  department_id INTEGER,
   PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
@@ -26,12 +26,13 @@ CREATE TABLE employee (
   role_id INTEGER,
   manager_id INTEGER,
   PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
 
 SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
+
+Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails (`employee_db`.`employee`, CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE)
 
 
--- seed.sql
+++ seed.sql
@@ -14,47 +14,48 @@ INSERT INTO department (name)
 VALUES ("Legal");
 
 
-/* Adding employees into the table: employee */
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Alex", "Varela", 3, null);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Stiglitz", 4, 1);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jen", "Koontz", 4, 1);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Caleb", "Barnes", 1, null);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("James", "Rolex", 2, 4);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Drew", "Albacore", 7, null);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Sandra", "Nanners", 6, 6);
-
-
 /* Adding roles into the table: role */
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 60000, 1);
+VALUES ("Sales Lead", 60000.00, 1);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Salesperson", 50000, 1);
+VALUES ("Salesperson", 50000.00, 1);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", 75000, 3);
+VALUES ("Lead Engineer", 75000.00, 3);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer", 65000, 3);
+VALUES ("Software Engineer", 65000.00, 3);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 65000, 2);
+VALUES ("Accountant", 65000.00, 2);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lawyer", 60000, 4);
+VALUES ("Lawyer", 60000.00, 4);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Legal Team Lead", 70000, 4);
+VALUES ("Legal Team Lead", 70000.00, 4);
+
+
+/* Adding employees into the table: employee */
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Alex", "Varela", 3, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Stiglitz", 4, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jen", "Koontz", 4, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Caleb", "Barnes", 1, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("James", "Rolex", 2, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Drew", "Albacore", 7, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sandra", "Nanners", 6, null);
+
